15e46fd1685f7b08f180dc4e2ced378890655bbe
Breania Harvey <harvey.breania@gmail.com>
2021-09-16 23:40:53 -0500

    displayCurrent function updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,35 @@
 const weatherApiRootUrl = 'https://api.openweathermap.org';
 const weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+const city = document.querySelector('#searchTerm').value;
+const todayEl = document.querySelector('#today');
+const now = new Date();
+function formatDate(date) {
+  var year = date.getFullYear();
+
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : '0' + month;
+
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : '0' + day;
+  return month + '/' + day + '/' + year;
+}
+
 
 function displayCurrent(current) {
+  let cityTitle = document.createElement("h3");
+  cityTitle.setAttribute("class", "title");
+  cityTitle.textContent = `${city} (${formatDate(now)})`;
+  todayEl.appendChild(cityTitle);
+
+  let weatherDetails = document.createElement("p")
+  weatherDetails.setAttribute("class", "details")
+  weatherDetails.textContent = 
+    `Temp: ${current.temp}
+    Wind: ${current.wind_speed}
+    Humidity: ${current.humidity}
+    UV Index: ${current.uvi}`
+  cityTitle.appendChild(weatherDetails);
+
 //Use jQuery to add to here:
 // <section
    //           id="today"
@@ -16,11 +44,10 @@ function displayFiveDay(daily) {
 }
 
 function saveToLocalState(city) {
-
+    localStorage.setItem("city", city);
 }
 
 function searchCityWeather() {
-  const city = document.querySelector('#searchTerm').value;
   fetch(`${weatherApiRootUrl}/geo/1.0/direct?q=${city}&limit=5&appid=${weatherApiKey}`)
   .then(function (res) {
     return res.json();
